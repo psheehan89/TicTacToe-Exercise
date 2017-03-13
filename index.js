e87e9956e702c9ec82f8c13a5ff7849d27ea6748
Patrick Sheehan <psheehan89@gmail.com>
2017-03-13 10:24:07 -0700

    (Initial Commit) Define a high level overview of what the project entails Very broad comments explaining the goal

---
+++ index.js
@@ -0,0 +1,9 @@
+//Create a board in the console
+//Define positions on the board (rows / columns)
+
+//Repeat these
+  //Indicate who's turn it is
+  //Take input from the current player
+  //Update the board to include the players move
+
+//Once a player has 3 in a row, declare that player the winner
