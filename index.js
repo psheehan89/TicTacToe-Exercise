a7580b40c52f2b74cf8e7f09ed378dc15ac02787
Patrick Sheehan <psheehan89@gmail.com>
2017-03-13 11:58:37 -0700

    Redesigned player schema

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ var move = {
 }
 
 //Define players, turns, and marks
-var players = [1, 2]
+var players = [
+  { name: 1,
+    markers: []
+  }, 
+  { name: 2,
+    markers: []
+  }]
 var turn = 0;
 var mark = ['X', 'O']
 
@@ -21,7 +27,7 @@ var board =          ' ' + move.topLeft + ' | ' + move.topMid + ' | ' + move.top
     '  \n-----------\n ' + move.midLeft + ' | ' + move.midMid + ' | ' + move.midRight + 
     '  \n-----------\n ' + move.botLeft + ' | ' + move.botMid + ' | ' + move.botRight + '\n\n'
 
-console.log("INSTRUCTIONS: type makeAMove('row col') in order to place your mark where you have\nrows: top, mid, bot\ncols: left, mid, right\n\nIt's player " + players[turn] + "'s turn ("+ mark[turn] +")\n")
+console.log("It's player " + players[turn].name + "'s turn ("+ mark[turn] +")\n")
 console.log(board)
 
 //Create a move function
@@ -30,35 +36,35 @@ function makeAMove(position) {
   //Take input from the current player
   switch(position) {
     case 'top left': 
-      move.topLeft === ' ' ? move.topLeft = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.topLeft === ' ' ? move.topLeft = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'top mid': 
-      move.topMid === ' ' ? move.topMid = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.topMid === ' ' ? move.topMid = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'top right': 
-      move.topRight === ' ' ? move.topRight = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.topRight === ' ' ? move.topRight = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'mid left': 
-      move.midLeft === ' ' ? move.midLeft = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.midLeft === ' ' ? move.midLeft = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'mid mid': 
-      move.midMid === ' ' ? move.midMid = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.midMid === ' ' ? move.midMid = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'mid right': 
-      move.midRight === ' ' ? move.midRight = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.midRight === ' ' ? move.midRight = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'bot left': 
-      move.botLeft === ' ' ? move.botLeft = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.botLeft === ' ' ? move.botLeft = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'bot mid': 
-      move.botMid === ' ' ? move.botMid = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.botMid === ' ' ? move.botMid = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     case 'bot right': 
-      move.botRight === ' ' ? move.botRight = mark[turn] : players[turn] === 1 ? turn = 1 : turn = 0;
+      move.botRight === ' ' ? move.botRight = mark[turn] : players[turn].name === 1 ? turn = 1 : turn = 0;
       break;
     default:
       console.log("That's not a valid move, please refer to the instructions")
-      players[turn] === 1 ? turn = 1 : turn = 0;
+      players[turn].name === 1 ? turn = 1 : turn = 0;
   }
 
   //Update the board to include the players move
@@ -68,12 +74,17 @@ function makeAMove(position) {
 
 
   //Indicate who's turn is next
-  players[turn] === 1 ? turn = 1 : turn = 0;
-  console.log("It's player " + players[turn] + "'s turn ("+ mark[turn] +")\n")
+  players[turn].name === 1 ? turn = 1 : turn = 0;
+  console.log("It's player " + players[turn].name + "'s turn ("+ mark[turn] +")\n")
 
+  //Print the board
   console.log('\n' + board)
+
+  //Once a player has 3 in a row, declare that player the winner
+  function didAnyoneWin(player) {
+  
+  }
 }
 
-//Once a player has 3 in a row, declare that player the winner
 
 
